63b46e9ecff195ffd55094e1580e75420fdef621
Carlos Cancino-Chacon <carlos.cancino@ofai.at>
2019-10-29 19:09:02 +0100

    add notebook for predictive models

--- basismixer/predictive_models/base.py
+++ basismixer/predictive_models/base.py
@@ -40,11 +40,15 @@ class FullPredictiveModel(nn.Module):
     """Meta model for predicting an expressive performance
     """
 
-    def __init__(self, model_configs, input_names, output_names,
+    def __init__(self, models_or_configs, input_names, output_names,
                  default_values={},
                  overlapping_output_strategy='FIFO'):
         super().__init__()
-        self.models = nn.ModuleList([construct_model(cfg) for cfg in model_configs])
+
+        if isinstance(models_or_configs[0], nn.Module):
+            self.models = models_or_configs
+        elif isinstance(models_or_configs[0], dict):
+            self.models = nn.ModuleList([construct_model(cfg) for cfg in models_or_configs])
         self.input_names = np.array(input_names)
         self.output_names = np.array(output_names)
         self.default_values = default_values
