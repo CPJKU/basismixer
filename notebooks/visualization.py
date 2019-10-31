34b10fff788e7a445e504547caa4c2a01880721e
Maarten Grachten <maarten.grachten@ofai.at>
2019-10-31 02:31:32 +0100

    update visualization notebook

--- notebooks/visualization.py
+++ notebooks/visualization.py
@@ -29,7 +29,9 @@ data.init()
 LOGGER = logging.getLogger(__name__)
 
 OGG_URL_BASE = 'https://spocs.duckdns.org/vienna_4x22/'
-PERF_CODEC = pc.PerformanceCodec(pc.TimeCodec(), pc.NotewiseDynamicsCodec())
+# PERF_CODEC = pc.PerformanceCodec(pc.TimeCodec(), pc.NotewiseDynamicsCodec())
+PERF_CODEC = pc.PerformanceCodec(pc.TimeCodec(normalization='bp_standardized'),
+                                 pc.OnsetwiseDecompositionDynamicsCodec())
 
 plt.rcParams.update({'font.size': 8})
 
@@ -44,7 +46,7 @@ def load_performance_audio(piece, performer):
         return None, None
 
 
-def get_performance_info(piece, performer, fig, axs):
+def get_performance_info(piece, performer):
     assert data.DATASET_DIR
     musicxml_fn = os.path.join(data.DATASET_DIR, 'musicxml', '{}.musicxml'.format(piece))
     match_fn = os.path.join(data.DATASET_DIR, 'match', '{}_{}.match'.format(piece, performer))
@@ -52,11 +54,13 @@ def get_performance_info(piece, performer, fig, axs):
     part = partitura.load_musicxml(musicxml_fn)
     
     ppart, alignment = partitura.load_match(match_fn, first_note_at_zero=True)
+    return part, ppart, alignment
 
+
+def show_performance(piece, performer, fig, axs, keep_zoom):
+    part, ppart, alignment = get_performance_info(piece, performer)
     # targets, mbp = PERF_CODEC.encode(matched_score)
     targets, snote_ids = PERF_CODEC.encode(part, ppart, alignment)
-    targets = targets.view(np.float32).reshape((len(targets), -1))
-    targets[np.isnan(targets)] = 0
 
     part_by_id = dict((n.id, n) for n in part.notes_tied)
     ppart_by_id = dict((n['id'], n) for n in ppart.notes)
@@ -74,8 +78,8 @@ def get_performance_info(piece, performer, fig, axs):
     # LOGGER.warning(f'{measure_times}')
     measure_times['t'] = bm(measure_times['t'])
     # LOGGER.warning(f'{measure_times}')
-    plot_targets(fig, axs, targets, onsets=s_onsets, xlabel='Measure number',
-                 xticks=measure_times) # , title='{} {}'.format(piece, performer))
+    make_plot(fig, axs, targets, onsets=s_onsets, xlabel='Measure number',
+              xticks=measure_times, keep_zoom=keep_zoom) # , title='{} {}'.format(piece, performer))
 
     s_times = np.r_[s_onsets, s_notes[-1].end.t]
     p_times = np.r_[p_onsets, p_notes[-1]['note_off']]
@@ -85,12 +89,20 @@ def get_performance_info(piece, performer, fig, axs):
     return score_perf_map
 
 
-def plot_targets(fig, axs, targets, onsets=None, xticks=None, title=None,
-                 xlabel=None, start=None, end=None):
+def make_plot(fig, axs, targets, onsets=None, xticks=None, title=None,
+                 xlabel=None, start=None, end=None, keep_zoom=False):
+    names = targets.dtype.names
+
+    xlims = []
+    ylims = []
     for ax in axs:
+        if keep_zoom:
+            xlims.append(list(ax.get_xlim()))
+            ylims.append(list(ax.get_ylim()))
+            # LOGGER.warning(f'keeping {xlims[-1]} {ylims[-1]}')
         ax.clear()
-    
-    n_targets = targets.shape[1]
+
+    n_targets = len(names)
 
     if onsets is None:
         x = np.arange(len(targets))
@@ -117,25 +129,40 @@ def plot_targets(fig, axs, targets, onsets=None, xticks=None, title=None,
 
     if title:
         fig.suptitle(title)
+
+    by_onset = partition(lambda ix: ix[1], enumerate(x))
+    for k, v in by_onset.items():
+        by_onset[k] = np.array([i for i, _ in v])
+
+    # LOGGER.warning(f'{by_onset}')
+    for i, name in enumerate(names):
+        target = targets[name]
+        targets[np.isnan(target)] = 0
         
-    for i, name in enumerate(PERF_CODEC.parameter_names):
-        axs[i].plot(x, targets[:, i], '.', label=name)
+        axs[i].plot(x, target, '.', label=name)
 
         if xticks is not None:
             axs[i].set_xticks(xticks['t'])
             axs[i].set_xticklabels(xticks['label'])
             axs[i].xaxis.grid()
 
-        by_onset = partition(lambda ix: ix[1], enumerate(x))
         tt = []
         vv = []
         for t, v in by_onset.items():
             tt.append(t)
-            vv.append(np.mean([targets[j, i] for j, _ in v]))
+            vv.append(np.mean(target[v]))
+            # LOGGER.warning(f'{t} {v} {np.mean(target[v])}')
         # axs[i].plot(tt, vv, label='{} (mean)'.format(name))
         axs[i].plot(tt, vv)
         
         axs[i].legend(frameon=False, loc=2)
+
+    if keep_zoom:
+        axs[0].set_xlim(xlims[0])
+        for xlim, ylim, ax in zip(xlims, ylims, axs):
+            # LOGGER.warning(f'setting {xlims[-1]} {ylims[-1]}')
+            ax.set_ylim(ylim)
+
     return fig, axs
 
 
@@ -143,6 +170,13 @@ def performance_player():
     status = widgets.Output()
     piece_dd = widgets.Dropdown(options=data.PIECES, description='Piece:')
     performer_dd = widgets.Dropdown(options=data.PERFORMERS, description='Performer:')
+    keep_lims_chbox = widgets.Checkbox(value=False, description='Keep zoom')
+    reset_lims = widgets.Button(description='Zoom to fit',
+                                button_style='', # 'success', 'info', 'warning', 'danger' or ''
+                                tooltip='Zoom to fit',
+                                icon='check'
+                                )
+
 
     if data.PIECES and data.PERFORMERS:
         current_performance = [data.PIECES[0], data.PERFORMERS[0]]
@@ -152,6 +186,8 @@ def performance_player():
     audio, fs = None, None
     score_perf_map = None
     aw = None
+    keep_zoom = False
+
     fig, axs = plt.subplots(len(PERF_CODEC.parameter_names),
                             sharex=True,
                             gridspec_kw={'hspace': 0.15})
@@ -167,17 +203,44 @@ def performance_player():
         
     def set_performance(piece, performer):
         nonlocal audio, fs, score_perf_map, aw
+
         audio, fs = load_performance_audio(piece, performer)
-        score_perf_map = get_performance_info(piece, performer, fig, axs)
-        if aw is None:
-            aw = display(Audio(data=audio, rate=fs, autoplay=True), display_id=True)
+        score_perf_map = show_performance(piece, performer, fig, axs, keep_zoom)
+
+        if keep_zoom:
+            s, e = axs[0].get_xlim()
+            start = max(0, int(score_perf_map(s)*fs))
+            end = min(len(audio), int(score_perf_map(e)*fs))
+            excerpt = audio[start:end]
         else:
-            aw.update(Audio(data=audio, rate=fs, autoplay=True))
-            
+            excerpt = audio
+        if aw is None:
+            aw = display(Audio(data=excerpt, rate=fs, autoplay=True), display_id=True)
+        else:
+            aw.update(Audio(data=excerpt, rate=fs, autoplay=True))
+
+    def set_keep_zoom(v):
+        nonlocal keep_zoom
+        keep_zoom = v['new']
+    
+    def do_reset_zoom(v):
+        nonlocal axs, fig
+        for ax in axs:
+            # ax.set_xlim()
+            # ax.set_ylim()
+            ax.autoscale()
+            ax.autoscale_view()
+            LOGGER.warning(f'{ax.get_xlim()}')
+            # ax.set_ylim()
+
+        fig.canvas.draw()  
+
     piece_dd.observe(partial(update_current_perf, item='piece'), names=['value'])
     performer_dd.observe(partial(update_current_perf, item='performer'), names=['value'])
+    keep_lims_chbox.observe(set_keep_zoom, names=['value'])
+    reset_lims.on_click(do_reset_zoom)
 
-    display(widgets.HBox([piece_dd, performer_dd]))
+    display(widgets.HBox([piece_dd, performer_dd, keep_lims_chbox, reset_lims]))
     display(status)
 
     set_performance(*current_performance)
@@ -229,10 +292,9 @@ def time_cursor_thread(fig, ax, start, end, duration, ev, rate=1):
     delta_t = 1/rate
 
     while not ev.is_set() and x < end:
-
+        fig.canvas.draw()
         vline.set(xdata=np.array([x, x]))
-        fig.canvas.blit(ax.bbox) # doesn't reliably update
-        # fig.canvas.draw()
+        # fig.canvas.blit(ax.bbox) # doesn't reliably update
         x += delta_x
         time.sleep(delta_t)
 
