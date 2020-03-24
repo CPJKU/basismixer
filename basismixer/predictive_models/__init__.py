e2fb0e671c38efd65d716cccfd8d6f3e6c5e8703
Carlos Cancino-Chacon <carlos.cancino@ofai.at>
2020-03-24 02:10:32 +0100

    update models

--- basismixer/predictive_models/__init__.py
+++ basismixer/predictive_models/__init__.py
@@ -5,7 +5,8 @@ Predictive Models
 from .base import (PredictiveModel,
                    NNModel,
                    FullPredictiveModel,
-                   construct_model)
+                   construct_model,
+                   get_nonlinearity)
 from .architectures import (FeedForwardModel,
                             RecurrentModel)
 from .train import NNTrainer, SupervisedTrainer, MSELoss
